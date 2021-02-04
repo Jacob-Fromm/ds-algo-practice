67198b48a823598587edec6810d9c4c4ba05c8c2
jakeFromm <jacobfromm@gmail.com>
2021-02-04 10:04:32 -0500

    frequency counter anagrams done

--- udemy-js-algo-data-structures/frequency-counter-code-along/anagrams.js
+++ udemy-js-algo-data-structures/frequency-counter-code-along/anagrams.js
@@ -1,1 +1,25 @@
 // Given two strings, write a function to determine if the second string is an anagram of the first. An anagram is a word, phrase, or name formed by rearranging the letters of another, such as cinema, formed from iceman.
+
+function validAnagram(first, second){
+    if (first.length !== second.length) {
+        return false
+    }
+
+    const lookup = {}
+
+    for (let i = 0; i < first.length; i++) {
+        let letter = first[i]
+        lookup[letter] ? lookup[letter] += 1 : lookup[letter] = 1
+    }
+    console.log(lookup)
+
+    for (let i = 0; i < second.length; i++) {
+        let letter = second[i]
+        if (!lookup[letter]) {
+            return false
+        } else {
+            lookup[letter] -= 1
+        }
+    }
+    return true
+}
